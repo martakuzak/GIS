740ec92881644189cbd8dda18d19ca63e974ff7e
Marta Kuzak <marta.kuzak@gmail.com>
2014-05-20 22:54:58 +0200

    drobna zmiana

--- src/Graph.java
+++ src/Graph.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 public class Graph {
 	private ArrayList<Vertex> vertices;
 	private ArrayList<Edge> edges;
+	private int colorNum; //liczba zuzytych kolorow
 	
 	public Graph() {
 		vertices = new ArrayList<>();
 		edges = new ArrayList<>();
-		
+		colorNum = 0;
 	}
 	
 	public void addVertices(int vnumber) {
@@ -15,9 +16,12 @@ public class Graph {
 			vertices.add(new Vertex(i));
 	}
 	public void addEdge(int v1, int v2, int id) {
-		if(v1 < vertices.size() && v2 < vertices.size()) {//jesli graf ma wierzcholki tej krawedzi 
+		if(v1 < vertices.size() && v2 < vertices.size() && v1 != v2) {//jesli graf ma wierzcholki tej krawedzi , i nie jest to petla
 			Edge e = new Edge(vertices.get(v1), vertices.get(v2), id);
 			edges.add(e);
+			vertices.get(v1).addNeigbour(vertices.get(v2));
+			vertices.get(v2).addNeigbour(vertices.get(v1));
+
 		}
 	}
 	public int getDegree() {
